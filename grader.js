6c60eeea94e539211e7a005ac857b0d59202448b
Ubuntu <ubuntu@ip-172-31-37-203.us-west-2.compute.internal>
2013-07-10 18:33:00 +0000

    updated version

--- grader.js
+++ grader.js
@@ -24,6 +24,7 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var URL_DEFAULT = "http://intense-ocean-1726.herokuapp.com/";
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
@@ -34,6 +35,10 @@ var assertFileExists = function(infile) {
         process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
     }
     return instr;
+};
+
+var assertURLExists = function(url) {
+    return true;
 };
 
 var cheerioHtmlFile = function(htmlfile) {
@@ -65,9 +70,11 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url>', 'URL to be checked', clone(assertURLExists), URL_DEFAULT)
         .parse(process.argv);
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
+    console.log(program.url);
     console.log(outJson);
 } else {
     exports.checkHtmlFile = checkHtmlFile;
